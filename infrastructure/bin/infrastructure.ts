c4360551b6a4fe5a63c7f48becdacc0de6074634
Rigoberto Miranda <rmiranda.s@me.com>
2021-01-03 19:21:04 -0400

    Updating stacks

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { CodeDeployStack } from '../lib/codeDeploy';
+import { EC2Stack } from '../lib/ec2-stack';
 
-const env = { account: '817379289172', region: 'us-east-1' };
+const env = { 
+    account: '817379289172', 
+    region: 'us-east-1'
+};
 
-const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack', { env: env });
+const stage             = 'dev'; // 'dev | test | prod | etc.'
+const app               = new cdk.App();
+const ec2Stack          = new EC2Stack(app, 'PreviewServerEC2Stack', stage, { env: env });
+const codeDeployStack   = new CodeDeployStack(app, 'PreviewServerCodeDeployStack', stage, { env: env });
+
+/*  Adding Resources/Dependencies */
+codeDeployStack.serverDeploymentGroup.addAutoScalingGroup(ec2Stack.ec2AutoScalingGroup);
--- infrastructure/lib/ec2-stack.ts
+++ infrastructure/lib/ec2-stack.ts
@@ -9,22 +9,24 @@ import {
     InstanceClass, 
     InstanceSize, 
     AmazonLinuxImage,
+    SubnetType,
 } from "@aws-cdk/aws-ec2";
 import { AutoScalingGroup } from "@aws-cdk/aws-autoscaling";
+import { ApplicationTargetGroup, ApplicationProtocol, TargetType, ApplicationLoadBalancer } from "@aws-cdk/aws-elasticloadbalancingv2";
+import { Transform } from 'stream';
 
 /*
     Documentation:
     — https://docs.aws.amazon.com/cdk/api/latest/docs/aws-ec2-readme.html
+    - https://docs.aws.amazon.com/cdk/api/latest/docs/aws-autoscaling-readme.html
 */
 
 export class EC2Stack extends cdk.Stack {
 
     public ec2AutoScalingGroup: AutoScalingGroup;
 
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    constructor(scope: cdk.App, id: string, stage: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
-        const stage = 'dev';
 
         /* CodeDeploy Role */
         const serviceRole = new Role(this, `PreviewServerEC2SRole${stage}`, {
@@ -37,12 +39,15 @@ export class EC2Stack extends cdk.Stack {
             ]
         });
 
-        /* This imports the default VPC but you can also
-        specify a 'vpcName' or 'tags'. */
+        /* 
+            This imports the default VPC but you can also
+            specify a 'vpcName' or 'tags'. 
+        */
         const vpc = Vpc.fromLookup(this, 'PreviewServerDefaultVPC', {
             isDefault: true,
         });
 
+        /* Security Group */
         const ec2SecurityGroup = new SecurityGroup(this, `PreviewServerEC2SecurityGroup${stage}`, {
             vpc: vpc,
             description: "Allows HTTP/HTTPS access to EC2 Instance",
@@ -54,6 +59,40 @@ export class EC2Stack extends cdk.Stack {
         ec2SecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3000));
         ec2SecurityGroup.addIngressRule(Peer.anyIpv6(), Port.tcp(3000));
 
+        /* Application Target Group */
+        const appTargetGroup = new ApplicationTargetGroup(this, `PreviewServerAppTargetGroup${stage}`, {
+            protocol: ApplicationProtocol.HTTP,
+            port: 3000,
+            vpc: vpc,
+            targetType: TargetType.INSTANCE,
+            targetGroupName: `PreviewServerAppTargetGroup-${stage}`,
+            deregistrationDelay: cdk.Duration.seconds(120),
+            healthCheck: {
+                healthyHttpCodes: '200',
+                path: '/'
+            }
+        });
+
+        /* Application Load Balancer */
+        const appLoadBalancer = new ApplicationLoadBalancer(this, `PreviewServerAppLoadBalancer-${stage}`, {
+            loadBalancerName: `PreviewServerAppLoadBalancer-${stage}`,
+            vpc: vpc,
+            securityGroup: ec2SecurityGroup,
+            internetFacing: true,
+            vpcSubnets: {
+                onePerAz: true,
+                subnetType: SubnetType.PUBLIC
+            }
+        });
+
+        /* 3000 is the default Port on Next.js  */
+        appLoadBalancer.addListener('Next.js', {
+            port: 3000,
+            protocol: ApplicationProtocol.HTTP,
+            open: true,
+            defaultTargetGroups: [appTargetGroup]
+        });
+
 
         const ec2UserDataCommands = `
             #!/bin/bash
@@ -63,7 +102,7 @@ export class EC2Stack extends cdk.Stack {
             wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
             chmod +x ./install
             sudo ./install auto
-        `
+        `;
 
         this.ec2AutoScalingGroup = new AutoScalingGroup(this, `PreviewServerAutoScalingGroup${stage}`, {
             autoScalingGroupName: `Next.js Preview Server ${stage}`,
@@ -78,8 +117,8 @@ export class EC2Stack extends cdk.Stack {
             role: serviceRole,
             keyName: 'next-preview-server', // Manually Created
         });
-
-        this.ec2AutoScalingGroup.addUserData(ec2UserDataCommands)
+        this.ec2AutoScalingGroup.attachToApplicationTargetGroup(appTargetGroup);
+        this.ec2AutoScalingGroup.addUserData(ec2UserDataCommands);
 
         cdk.Tags.of(this.ec2AutoScalingGroup).add('name', 'next-preview-server', {
             applyToLaunchedInstances: true
